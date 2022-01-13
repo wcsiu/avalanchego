ee75522784f2de6752ba8c73a5443b1a02efe381
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2022-01-13 12:23:30 -0800

    nits

--- api/health/client.go
+++ api/health/client.go
@@ -57,13 +57,19 @@ func (c *client) Liveness(ctx context.Context) (*APIHealthReply, error) {
 }
 
 func (c *client) AwaitHealthy(ctx context.Context, freq time.Duration) (bool, error) {
+	// Check health once outside the loop to avoid sleeping unnecessarily.
+	res, err := c.Health(ctx)
+	if err == nil && res.Healthy {
+		return true, nil
+	}
+
 	for ctx.Err() == nil {
-		res, err := c.Health(ctx)
+		time.Sleep(freq)
+		res, err = c.Health(ctx)
 		if err == nil && res.Healthy {
 			return true, nil
 		}
-		time.Sleep(freq)
 	}
 
-	return false, nil
+	return false, err
 }
